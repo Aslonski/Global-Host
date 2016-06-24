734626337bceb94f38817e0cda042f2a4418e604
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-24 15:11:47 -0500

    Create initial views for conversations and messages

--- db/migrate/20160624184130_create_messages.rb
+++ db/migrate/20160624184130_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.text :body
-      t.integer :conversation
-      t.integer :user
+      t.integer :conversation_id
+      t.integer :user_id
       t.boolean :read
 
       t.timestamps(null: false)
