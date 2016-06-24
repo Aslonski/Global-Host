1be0a067570a258a2225ad02f0a7354539083efb
Gabriel Zurita <email": null>
2016-06-24 14:47:54 -0500

    Complete associations -- need to check if User self-join through foreign key is correct

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :user_interests
   has_many :interests, through: :user_interests
+
+  has_many :itineraries, foreign_key: "visitor_id"
+  has_many :itineraries, foreign_key: "host_id"
 end
