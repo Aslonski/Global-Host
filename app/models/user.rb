09a7462626ed1e677170069a849676061ce4c74b
Gabriel Zurita & Ryan Dempsey <pair+gabezurita.rmdemp@devbootcamp.com>
2016-06-24 16:09:15 -0500

    Complete implementation and testing of itinerary, activity, location.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :user_interests
   has_many :interests, through: :user_interests
 
-  has_many :itineraries, foreign_key: "visitor_id"
-  has_many :itineraries, foreign_key: "host_id"
+  has_many :visitor_itineraries, foreign_key: "visitor_id", class_name: "Itinerary"
+  has_many :host_itineraries, foreign_key: "host_id", class_name: "Itinerary"
 end
