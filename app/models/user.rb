a05d08d72fd9e6f02ef0fbdafda654b78cdcf5e2
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-24 18:06:48 -0500

    Complete message and conversation associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
 	has_many :interests, through: :user_interests
 	has_many :likeminded_users, { through: :interests, source: :users }
 
+  has_many :started_conversations, foreign_key: :sender_id, class_name: "Conversation"
+  has_many :continued_conversations, foreign_key: :recipient_id, class_name: "Conversation"
+  has_many :messages, through: :conversations
+
 	def self.hosts
 		where(is_host: true)
 	end
