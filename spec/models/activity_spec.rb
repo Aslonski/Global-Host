937550d2ba44d29122cef155a4d96b4c52872b4f
Ryan Dempsey <email": null>
2016-06-24 19:53:29 -0500

    Write tests for Activity model - all pass

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:activity) { Activity.new(description: "Here is a description.", itinerary_id: 1, location_id: 2) }
+
+  it "has a valid description" do
+    expect(activity.description).to eq("Here is a description.")
+  end
+
+  it "has a valid itinerary ID" do
+    expect(activity.itinerary_id).to eq(1)
+  end
+
+  it "has a valid location ID" do
+    expect(activity.location_id).to eq(2)
+  end
+
+  it { should belong_to(:itinerary) }
+
+  it { should belong_to(:location) }
+
 end
