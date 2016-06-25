232ec7270a1105f937a23dd0254a464ca1fcd8ea
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-24 19:06:56 -0500

    Write basic tests for conversation and message structure

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:patrick) {User.create!(first_name: "Patrick", last_name: "DeWitte", city: "Chicago", state_province: "IL", country: "USA", personal_info: "Is a cool guy", language: "English", gender: "male", is_host: false)}
+  let(:andrey) {User.create!(first_name: "Andrey", last_name: "Slonski", city: "Cincinatti", state_province: "OH", country: "USA", personal_info: "Is a cool guy too", language: "English", gender: "male", is_host: true)}
+
+  let(:conversation) {Conversation.create(sender: patrick, recipient: andrey)}
+
+  it "is started by one user" do
+    expect(conversation.sender).to eq patrick
+  end
+
+  it "is continued by another user" do
+    expect(conversation.recipient).to eq andrey
+  end
 end
