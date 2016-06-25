317ee2b64838f88f013f79dfb467e84e7015ea53
Gabriel Zurita & Ryan Dempsey <pair+gabezurita.rmdemp@devbootcamp.com>
2016-06-24 19:03:52 -0500

    Create note file explaining all associations; further testing of current associations - all working

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ActiveRecord::Base
   has_many :activities
   has_many :itineraries, through: :activities
-  has_many :travelers, through: :itineraries, source: "User"
+  has_many :travelers, through: :itineraries, source: :visitor
+  has_many :natives, through: :itineraries, source: :host
 end
