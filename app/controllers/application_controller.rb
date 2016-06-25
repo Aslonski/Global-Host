f7e5086006ee686715fffb962068a92cf3d891ee
[01;31m[K"name"[m[K: "Gabriel Zurita <--replace-all>
2016-06-25 10:18:56 -0500

    Add applicaiton current user to sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+  include SessionsHelper
 
-  def authorize
-    redirect_to new_session_path unless current_user
-  end
 end
