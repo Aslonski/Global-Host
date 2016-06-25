2b22f5686bb4c23c9974e5fc0cb0110c7057a89d
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-25 17:07:10 -0500

    commit and switch to message branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       @errors = user.errors.full_messages
