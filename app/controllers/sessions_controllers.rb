96146c5a8052fd6f6f5ef6a5ae2e034cdd5035fa
[01;31m[K"name"[m[K: "Gabriel Zurita <--replace-all>
2016-06-24 19:52:11 -0500

    Finish implementation of user creation/login/authentication

---
+++ app/controllers/sessions_controllers.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+    # If user's login doesn't work, send them back to the login form.
+      @errors = ["Email and/or password is incorrect!"]
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	has_many :user_interests
 	has_many :interests, through: :user_interests
 	has_many :likeminded_users, { through: :interests, source: :users }
+	
+	has_secure_password
 
 	def self.hosts
 		where(is_host: true)
