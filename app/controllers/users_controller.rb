ee955a4514a47b048e47ea8af683a0514429699f
Gabriel Zurita <gabezurita@gmail.com>
2016-06-26 12:49:08 -0500

    Complete implementation of redirect to users_search_path after logging in and registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params_registration_form)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to users_search_path
     else
       @errors = @user.errors.full_messages
       render :new
