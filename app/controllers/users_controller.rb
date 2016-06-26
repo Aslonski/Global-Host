1e3b2a9af1282e375e7d7acb2075d4f68f2a0fc8
Gabriel Zurita <gabezurita@gmail.com>
2016-06-26 11:11:07 -0500

    Complete implementation of login form -- fully functional and styled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_filter 'authorize', :only => [:edit, :delete]
 
   def new
+    @user = User.new
   end
 
   def create
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      @errors = user.errors.full_messages
+      @errors = @user.errors.full_messages
       render :new
     end
   end
@@ -27,6 +28,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :city, :state_province, :country, :personal_info, :language, :gender, :is_host, :password)
+    params.permit(:first_name, :last_name, :email, :city, :state_province, :country, :personal_info, :language, :gender, :is_host, :password)
   end
 end
