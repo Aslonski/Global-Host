eece4275b1f0cc4e597a342ced60c6cc4d19d36e
Gabriel Zurita <gabezurita@gmail.com>
2016-06-26 10:12:28 -0500

    Fixing new user form error: no user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user
