19402973b524630b5a6f2a430ba33be6f7e4e76b
Gabriel Zurita <gabezurita@gmail.com>
2016-06-25 20:20:54 -0500

    Progress on registration form. Need more css and to fix capturing value of host.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
 
 	has_secure_password
-	
+
   has_many :visitor_itineraries, foreign_key: "visitor_id", class_name: "Itinerary"
   has_many :host_itineraries, foreign_key: "host_id", class_name: "Itinerary"
 
 	has_many :user_interests
 	has_many :interests, through: :user_interests
 	has_many :likeminded_users, { through: :interests, source: :users }
-	
+
 	has_secure_password
 
   has_many :started_conversations, foreign_key: :sender_id, class_name: "Conversation"
