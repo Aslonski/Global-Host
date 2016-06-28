b150d1f3557bfca06376808dfd0e94af6d98e84f
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-27 21:06:25 -0500

    Animate pin drop

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,3 +1,9 @@
+// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDHNVo3T3tcjPrSGTh9ifxZy5m7Cnbozxs
+
+$(document).ready(function(){
+  var mapCenter = $('#itin-city').text()
+});
+
 var map;
 
 function initMap() {
@@ -5,15 +11,25 @@ function initMap() {
     center: {lat: 48.8566, lng: 2.3522},
     zoom: 8
   });
-
-  var myLatLng = {lat: 48.8867, lng: 2.3431}
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
 }
+
+// geocoder = new google.maps.Geocoder();
+// geocoder.geocode({ 'address': mapCenter}, function(results, status) {
+//   if (status == google.maps.GeocoderStatus.OK) {
+//     map.setCenter(results[0].geometry.location);
+//     console.log(results)
+//   }
+// });
+
+// Drop marker
+var myLatLng = {lat: 48.8867, lng: 2.3431}
+
+var marker = new google.maps.Marker({
+  position: myLatLng,
+  map: map,
+  animation: google.maps.Animation.DROP,
+  title: 'Hello World!'
+});
 
 // Geocode current location
 // var infoWindow = new google.maps.InfoWindow({map: map});
