e2bd6d2fa9ecfe18d7d929d2ca63fc8823f040bb
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-27 20:13:41 -0500

    Drop static pin on map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -5,30 +5,39 @@ function initMap() {
     center: {lat: 48.8566, lng: 2.3522},
     zoom: 8
   });
-}
 
-var infoWindow = new google.maps.InfoWindow({map: map});
+  var myLatLng = {lat: 48.8867, lng: 2.3431}
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latititude,
-      lng: position.coords.longitude
-    };
-
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    map.setCenter(pos);
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
   });
-} else {
-  handleLocationError(false, infoWindow, map.getCenter());
-  }
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browswer doesn\'t support geolocation.');
 }
+
+// Geocode current location
+// var infoWindow = new google.maps.InfoWindow({map: map});
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     var pos = {
+//       lat: position.coords.latititude,
+//       lng: position.coords.longitude
+//     };
+
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent('Location found.');
+//     map.setCenter(pos);
+//   }, function() {
+//     handleLocationError(true, infoWindow, map.getCenter());
+//   });
+// } else {
+//   handleLocationError(false, infoWindow, map.getCenter());
+//   }
+
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//     'Error: The Geolocation service failed.' :
+//     'Error: Your browswer doesn\'t support geolocation.');
+// }
