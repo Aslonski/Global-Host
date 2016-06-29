0eb441da2346812215bf204e67258f27605213cf
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-29 09:48:26 -0500

    Text box for map markers working

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,14 +1,6 @@
 var map;
 
 function initMap() {
-  function centerFinder(){
-    if (window.locationsCollection && (window.locationsCollection.length > 0)) {
-      return window.locationsCollection[0]
-    } else {
-      return window.mapCenterLocation
-    }
-  };
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: centerFinder(),
     zoom: 12
@@ -19,10 +11,30 @@ function initMap() {
   })
 }
 
+function centerFinder(){
+  if (window.locationsCollection && (window.locationsCollection.length > 0)) {
+    return window.locationsCollection[0]
+  } else {
+    return window.mapCenterLocation
+  }
+};
+
+
 function markerMaker(location){
+var infoWindow = new google.maps.InfoWindow({
+  content: "HEY FUCKFACE"
+});
   var marker = new google.maps.Marker({
     position: location,
     map: map,
     animation: google.maps.Animation.DROP
   });
+
+
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
 }
+
+
+
