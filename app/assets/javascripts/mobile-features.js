f9e11fa4b01db42cc64aba4581f809724c99abf6
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-29 15:32:48 -0500

    Commit before switch to general-improvements

--- app/assets/javascripts/mobile-features.js
+++ app/assets/javascripts/mobile-features.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+
   $('.modal-trigger').leanModal();
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 })
+
