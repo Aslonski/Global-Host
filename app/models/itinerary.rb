124ff867b144b344a6d349c80f4685ef19b620d8
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-28 19:48:29 -0500

    Start refactor of httparty methods

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -15,20 +15,28 @@ class Itinerary < ActiveRecord::Base
     @locationJsons = []
     locations.each do |location|
       formatted_address = location.address.split(' ').join('+')
+
       response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{formatted_address},+#{location.city}&key=#{ENV["GOOGLE_GEOCODE_KEY"]}")
       location_data = JSON.parse(response.body)
       @locationJsons << location_data["results"].first["geometry"]["location"]
     end
-    p @locationJsons
+    @locationJsons
   end
 
   def center_lat_lng
     return unless city
     return @center_lat_lng if @center_lat_lng
 
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{city}&key=#{ENV["GOOGLE_GEOCODE_KEY"]}")
+    # response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{city}&key=#{ENV["GOOGLE_GEOCODE_KEY"]}")
+    # location_data = JSON.parse(response.body)
+    # @center_lat_lng = location_data["results"].first["geometry"]["location"]
+    @center_lat_lng = get_geocode(city)
+  end
+
+  def get_geocode(location_info)
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{location_info}&key=#{ENV["GOOGLE_GEOCODE_KEY"]}")
     location_data = JSON.parse(response.body)
-    @center_lat_lng = location_data["results"].first["geometry"]["location"]
+    location_data["results"].first["geometry"]["location"]
   end
 
   def city
