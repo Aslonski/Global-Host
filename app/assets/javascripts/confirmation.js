ea65735b9c3e7398320f6d206a47cc603ce3c55b
Ryan Dempsey <email": null>
2016-06-28 20:39:02 -0500

    jQuery functionality for Itinerary confirmation works

--- app/assets/javascripts/confirmation.js
+++ app/assets/javascripts/confirmation.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 
   $(".confirm-button").on("click", function(event){
-    event.preventDefault();
-    console.log("howdy");
-    $(".edit-button").removeClass("hidden");
     $(this).addClass("hidden");
-    $(".update-activity-button").addClass("hidden");
-    $(".remove-activity-button").addClass("hidden");
+    $(".edit-button").removeClass("hidden");
+    $(".activity-buttons").addClass("hidden");
+
   })
 
+  $(".edit-button").on("click", function(){
+    $(this).addClass("hidden");
+    $(".confirm-button").removeClass("hidden");
+    $(".activity-buttons").removeClass("hidden");
+  })
 
 })
